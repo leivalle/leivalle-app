0d8f53e72217c0ed3d15658c8e3f6fc30ca389c8
Anton Rautio <anton.rautio@smartly.io>
2019-08-02 10:32:42 +0300

    Add base for ranking page

--- leivalle/src/components/ranking.js
+++ leivalle/src/components/ranking.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import UserRank from './user_rank';
 import 'react-bootstrap'
-import { ListGroup } from 'react-bootstrap';
+import '../styles/ranking.css';
+import { ListGroup, Jumbotron } from 'react-bootstrap';
 function Ranking() {
   return (
-    <div>
+    <div className='rankingBody'>
+      <Jumbotron>
         <ListGroup>
           <ListGroup.Item><UserRank name='Martti'/></ListGroup.Item>
           <ListGroup.Item><UserRank name='Testo'/></ListGroup.Item>
           <ListGroup.Item><UserRank name='Riku'/></ListGroup.Item>
         </ListGroup>
+      </Jumbotron>
     </div>
   );
 }
--- leivalle/src/index.js
+++ leivalle/src/index.js
@@ -7,6 +7,8 @@ import Ranking from './components/ranking';
 import Input from './components/input';
 import * as serviceWorker from './serviceWorker';
 
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
 ReactDOM.render((
     <BrowserRouter>
         <Route exact path='/' component={Login}/>
